46617ba1fcda96920d2fcf0645931644742f6eef
Maira Galvao <mairagalvao01@gmail.com>
2025-04-28 19:46:07 +0300

    feat:created filter tool bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import TopNavBar from "./Components/TopNavBar";
 
 import { Box } from "@mui/material";
 import Footer from "./Components/Footer";
+import FilterToolbar from "./Components/FilterToolbar";
 
 const App: React.FC = () => {
   return (
     <>
       <Box>
         <TopNavBar />
-
+        <FilterToolbar />
         <UserCardContainer />
         <Footer />
       </Box>
---
+++ src/Components/FilterToolbar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { AppBar, Toolbar, IconButton, Box, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import LockIcon from "@mui/icons-material/Lock";
+
+const FilterToolbar: React.FC = () => {
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "transparent",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingLeft: 2,
+          paddingRight: 2,
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <IconButton>
+            <SearchIcon sx={{ color: "black" }} />
+          </IconButton>
+
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: "bold",
+                color: "black",
+                textDecoration: "underline",
+                textDecorationColor: "orange",
+              }}
+            >
+              Joined
+            </Typography>
+            <Typography variant="body2" sx={{ color: "gray" }}>
+              Created
+            </Typography>
+          </Box>
+        </Box>
+
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+          <LockIcon sx={{ color: "dodgerblue" }} />
+          <Typography variant="body2" sx={{ color: "lightgray" }}>
+            = Private
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default FilterToolbar;

2ed1a24100fc2bd64e3631c5ad93e344038ed8d5
Maira Galvao <mairagalvao01@gmail.com>
2025-04-28 19:28:20 +0300

    feat:created navbar

--- src/Components/TopNavBar.tsx
+++ src/Components/TopNavBar.tsx
@@ -1,23 +1,58 @@
 import React from "react";
-import { AppBar, Toolbar, IconButton, Typography, Box } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Box } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import MenuIcon from "@mui/icons-material/Menu";
+import AddIcon from "@mui/icons-material/Add";
+
 import tripteamLogo from "../tripteamLogo.jpeg";
 
 const TopNavBar: React.FC = () => {
   return (
-    <Box>
-      <AppBar position="static" color="transparent" elevation={0}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="logo"
-            sx={{ mr: 2 }}
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+    >
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        {/* Left: Logo */}
+        <IconButton>
+          <img src={tripteamLogo} alt="Logo" style={{ height: 40 }} />
+        </IconButton>
+
+        {/* Right: Icons */}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            gap: 2,
+          }}
+        >
+          <SearchIcon sx={{ color: "dodgerblue", fontSize: 30 }} />
+          <Box
+            sx={{
+              bgcolor: "dodgerblue",
+              borderRadius: "50%",
+              width: 30,
+              height: 30,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <img src={tripteamLogo} alt="Logo" style={{ height: 40 }} />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <AddIcon sx={{ color: "white" }} />
+          </Box>
+          <FavoriteIcon sx={{ color: "red", fontSize: 30 }} />
+          <MenuIcon sx={{ color: "black", fontSize: 30 }} />
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
